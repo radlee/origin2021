e624b71011718c7b61836c96bf8ffd0ec80ddf6d
radlee <prof.lolito@gmail.com>
2020-03-29 01:57:13 +0200

    Updates

---
+++ precache-manifest.0428b2d888c473aa403ac02fe8b09ee3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "250be351b91111286d9985aa431e7f42",
+    "url": "/origin2020/index.html"
+  },
+  {
+    "revision": "76d23e52ff0f851eff36",
+    "url": "/origin2020/static/css/main.29f6bd39.chunk.css"
+  },
+  {
+    "revision": "758becc78615656624a0",
+    "url": "/origin2020/static/js/2.01a2d42d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/origin2020/static/js/2.01a2d42d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76d23e52ff0f851eff36",
+    "url": "/origin2020/static/js/main.b90ba17c.chunk.js"
+  },
+  {
+    "revision": "c76f5a414f79bfcb3aa1",
+    "url": "/origin2020/static/js/runtime-main.bfe6ba8a.js"
+  }
+]);
